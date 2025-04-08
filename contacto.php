a3b591d600a3ae6723b6bcf916ccb6265f2893ef
Alexis <alexis_marrero_22@hotmail.com>
2025-04-08 11:28:20 +0100

    pagina de contacto y politica de privacidad creado

---
+++ contacto.php
@@ -0,0 +1,22 @@
+<?php require_once("views/cabecera.php"); ?>
+<h1>¡Ponte en contacto con nosotros!</h1>
+<h3 style="text-align: center;">¿Tienes dudas, sugerencias o simplemente quieres hablarnos de tu coche slot favorito?</h3>
+<h3 style="text-align: center;">¡Estaremos encantados de leerte!</h3>
+<form action="index.php" method="post">
+    <label for="nombre">Tu nombre de piloto:</label>
+    <input type="text" id="nombre" name="nombre"><br><br>
+
+    <label for="email">Tu email de boxes:</label>
+    <input type="email" id="email" name="email"><br><br>
+
+    <label for="mensaje">Tu mensaje de rally</label><br>
+    <textarea id="mensaje" name="mensaje" rows="4" cols="50"></textarea><br><br>
+
+    <button type="submit">Enviar 🚗💨</button>
+</form>
+<p style="margin-top:30px; text-align:center;">
+    <i><b>🏁 Gracias por contactar con Car Slot Collection.  
+    Prometemos no usar tus datos para cronometrar vueltas... 😉</b></i>
+</p>
+
+<?php require_once("views/pieDePagina.php"); ?>
